473ad87a508ecd468c1c40e11a2fd05dc2e6db0e
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2025-07-15 01:24:29 +0300

    minor edits

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: ['https://comsierge-frontend.onrender.com'],
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
